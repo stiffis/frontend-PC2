ad157ff5b105dff731a75d2f2527f16084d3f4cc
stiffis <steve.ildefonso@utec.edu.pe>
2025-06-19 08:51:13 -0500

    feat: add template

---
+++ src/components/TwoColumns.tsx
@@ -0,0 +1,14 @@
+export default function TwoColumns() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-mocha-base">
+      <div className="flex gap-8">
+        <div className="bg-mocha-blue rounded-2xl p-8 text-2xl border-8 shadow-md w-64 flex items-center justify-center text-mocha-peach">
+          hola
+        </div>
+        <div className="bg-mocha-blue rounded-2xl p-8 text-2xl border-8 shadow-md w-64 flex items-center justify-center text-mocha-peach">
+          addison
+        </div>
+      </div>
+    </div>
+  );
+}
